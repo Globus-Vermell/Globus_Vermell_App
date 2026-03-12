1032de8e40271d6d464e9f0175ff9d908a5c0027
denisitoow <denis.coello.7e8@itb.cat>
2026-03-12 09:59:39 +0100

    Añadir SharedPreferences para idioma i modos de vista

--- lib/providers/language_provider.dart
+++ lib/providers/language_provider.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LanguageProvider with ChangeNotifier {
   Locale _currentLocale = const Locale('ca');
-
   Locale get currentLocale => _currentLocale;
-  void changeLanguage(String languageCode) {
+
+  LanguageProvider() {
+    _loadFromPrefs();
+  }
+
+  void changeLanguage(String languageCode) async {
     _currentLocale = Locale(languageCode);
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('languageCode', languageCode);
+  }
+
+  void _loadFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? languageCode = prefs.getString('languageCode');
+    if (languageCode != null) {
+      _currentLocale = Locale(languageCode);
+      notifyListeners();
+    }
   }
 }
--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:globus_vermell_app/theme/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
   bool _isDark = false;
@@ -10,47 +11,55 @@ class ThemeProvider with ChangeNotifier {
   bool get isHighContrast => _isHighContrast;
   bool get isColorBlind => _isColorBlind;
 
+  ThemeProvider() {
+    _loadFromPrefs();
+  }
+
+  Future<void> _saveToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDark', _isDark);
+    await prefs.setBool('isHighContrast', _isHighContrast);
+    await prefs.setBool('isColorBlind', _isColorBlind);
+  }
+
+  /*
+  Cuándo un modo esté activo veremos si el tema es blanco o negro y activaremos sus respectivos temas.
+   */
   ThemeData get themeData {
-    // 1. Si el modo alto contraste está activo
     if (_isHighContrast) {
-      // Devolvemos la versión oscura o clara dependiendo del switch de Dark Mode
       return _isDark ? AppThemes.darkHighContrastMode : AppThemes.highContrastMode;
     }
-
-    // 2. Si el modo daltonismo está activo
     if (_isColorBlind) {
-      // Devolvemos la versión oscura o clara dependiendo del switch de Dark Mode
       return _isDark ? AppThemes.darkColorBlindMode : AppThemes.colorBlindMode;
     }
-
-    // 3. En caso de no tener modos de adaptabilidad, elegimos los modos normales
     return _isDark ? AppThemes.darkMode : AppThemes.lightMode;
   }
 
   void toggleTheme() {
     _isDark = !_isDark;
-    // ¡Ojo al dato bb! Ya NO apagamos los otros modos aquí. 
-    // Queremos que el modo oscuro se sume a la fiesta, ¡owo! ✨
+    _saveToPrefs();
     notifyListeners();
   }
 
   void toggleHighContrast() {
     _isHighContrast = !_isHighContrast;
-    if (_isHighContrast) {
-      // Apagamos el daltonismo para que no choquen entre sí,
-      // ¡pero dejamos el Dark Mode tranquilo para que se combinen!
-      _isColorBlind = false;
-    }
+    if (_isHighContrast) _isColorBlind = false;
+    _saveToPrefs();
     notifyListeners();
   }
 
   void toggleColorBlind() {
     _isColorBlind = !_isColorBlind;
-    if (_isColorBlind) {
-      // Apagamos el alto contraste,
-      // ¡pero dejamos el Dark Mode en paz!
-      _isHighContrast = false;
-    }
+    if (_isColorBlind) _isHighContrast = false;
+    _saveToPrefs();
+    notifyListeners();
+  }
+
+  void _loadFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDark = prefs.getBool('isDark') ?? false;
+    _isHighContrast = prefs.getBool('isHighContrast') ?? false;
+    _isColorBlind = prefs.getBool('isColorBlind') ?? false;
     notifyListeners();
   }
 }
